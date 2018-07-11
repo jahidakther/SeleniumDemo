1347aa64f8ceba6f898a76ff305f65267c1c9b4f
tuba <tuba@tuba-PC>
2018-07-11 06:52:44 -0400

    updated on 6:52 am 7/11

---
+++ KatalonDemo/src/testCases/TC1_3VerifyAppointmentPage.java
@@ -0,0 +1,49 @@
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.LogStatus;
+
+import pageObject.AppointmentPage;
+import pageObject.HomePage;
+import pageObject.LoginPage;
+import utilities.InitialConfiguration;
+import utilities.ScreenShot;
+
+public class TC1_3VerifyAppointmentPage extends InitialConfiguration{
+
+	//Create Object for all pages
+		LoginPage login=new LoginPage(driver);
+		HomePage home=new HomePage(driver);
+		AppointmentPage appPage=new AppointmentPage(driver);
+		
+		
+		//Test Cases
+		@Test(priority=0)
+		public void verifyPageLabel() {
+			
+			    driver.findElement(home.getBurgerMenuLocator()).click();
+		        driver.findElement(home.getloginLocator()).click();
+		
+			        extent.startTest("TC01.3  Navigate to Appointment Page", "Appointment Page");
+			       
+		
+			try {
+				
+			    extent.log(LogStatus.PASS, "Navigate to Appointment Page",
+						"View details below:",
+						ScreenShot.captureScreen(driver, "Appointment Page "+timeStamp));
+			}catch(Exception e) {
+								extent.log(LogStatus.FAIL, "Fail to Navigate Appointment Page",
+						"View details below:",
+						ScreenShot.captureScreen(driver, "Appointment Page "+timeStamp));
+		
+			}
+			extent.endTest();
+			
+		}
+		
+			
+	
+}
